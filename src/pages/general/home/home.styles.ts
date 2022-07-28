812b5bd1b5ba0712a795d3d91a85f8f270b30d43
bolodissenoura <suamaemedeu@outlook.com>
2022-07-28 14:25:35 -0300

    fix: styles from bkg banner

--- src/pages/general/home/home.styles.ts
+++ src/pages/general/home/home.styles.ts
@@ -3,10 +3,13 @@ import img from '/assets/img/bkg-1.png';
 
 export const Container = styled.div`
   height: 100vh;
-  width: 100%;
-  background-image: url(${img});
-  background-position: top;
+  width: 100vw;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${img});
+  /* Position and center the image to scale nicely on all screens */
+  background-position: center;
   background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const Divider = styled.div`
