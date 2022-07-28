fcc7b9bfecb3597b600fecd70340d81529f950d7
bolodissenoura <suamaemedeu@outlook.com>
2022-07-28 13:24:03 -0300

    feat: container banner with im copmponent

--- src/pages/general/home/home.page.tsx
+++ src/pages/general/home/home.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import * as C from '../../../components';
+import * as S from './home.styles';
 
 export function Home() {
   return (
     <>
       <C.Header />
+      <S.Container></S.Container>
       <h1>Home</h1>
     </>
   );
---
+++ src/pages/general/home/home.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import img from '../../../../public/assets/img/bkg-1.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: url(${img});
+  background-position: top;
+  background-repeat: no-repeat;
+`;
