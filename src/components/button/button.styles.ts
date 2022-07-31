6e93f713bb20abe1542ea0d3559c8b0bd770bdb7
bolodissenoura <suamaemedeu@outlook.com>
2022-07-31 19:13:26 -0300

    feat: button styles hover

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -2,11 +2,18 @@ import styled from 'styled-components';
 
 export const Content = styled.button`
   width: 300px;
-  background-color: transparent;
+  cursor: pointer;
+  color: var(--grayLight);
   border: solid var(--grayLight);
+  background-color: transparent;
+  transition: all 0.5s ease;
+
+  :hover {
+    background-color: var(--grayLight);
+    color: var(--black);
+  }
 `;
 
 export const Text = styled.h1`
   margin: 10px;
-  color: var(--grayLight);
 `;
