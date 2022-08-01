a2f940ce02eb6f46ea5d6bef644796d70acacbe9
bolodissenoura <suamaemedeu@outlook.com>
2022-07-31 22:09:13 -0300

    feat: third section content

--- src/pages/general/home/home.page.tsx
+++ src/pages/general/home/home.page.tsx
@@ -20,17 +20,49 @@ export function Home() {
       <S.SecondContainer>
         <C.TransitionBox />
         <S.AboutUsContainer>
-          <C.Title text='Sobre Nossa Associação' />
-          <C.SubTitle
-            text='Somos uma organização de ajuda a animais em situação de rua e
+          <S.AboutUsFirstContainer>
+            <C.Title text='Sobre Nossa Associação' />
+            <C.SubTitle
+              text='Somos uma organização de ajuda a animais em situação de rua e
             desabrigados, fundada em 2012, pelo nosso amigo e voluntário
             Virgílio Martins. Desde que começamos com este trabalho, nosso
             objetivo é ajudar o máximo possível de cães. Nós os resgatamos,
             vacinamos, vermigugamos, desparasitamos, damos uma estadia digna e
             tentamos dar uma família a todos.'
-          />
+            />
+          </S.AboutUsFirstContainer>
+          <S.AboutUsSecondContainer>
+            <S.ColText>
+              <C.Title text='Sobre Nossa Associação' />
+              <C.SubTitle
+                text='Somos uma organização de ajuda a animais em situação de rua e
+            desabrigados, fundada em 2012, pelo nosso amigo e voluntário
+            Virgílio Martins. Desde que começamos com este trabalho, nosso
+            objetivo é ajudar o máximo possível de cães. Nós os resgatamos,
+            vacinamos, vermigugamos, desparasitamos, damos uma estadia digna e
+            tentamos dar uma família a todos.'
+              />
+            </S.ColText>
+            <S.Col>
+              <S.UsImg
+                src='../../../../public/assets/img/us.png'
+                alt='Fotografia com um grupo de pessoas de nossa organização'
+              />
+            </S.Col>
+          </S.AboutUsSecondContainer>
         </S.AboutUsContainer>
       </S.SecondContainer>
+      <S.ThirdContainer>
+        <S.ToAdopteContainer>
+          <C.Title text='APADRINHE UM CÃOZINHO' />
+          <C.SubTitle text='O apadrinhamento pet é uma bela opção para quem ainda não pode ter um pet em casa ou não consegue aumentar a família no momento, mas morre de vontade de ajudar ONGs que atuam na área de proteção animal. ' />
+          <S.ToAdopteImg
+            src='../../../../public/assets/img/dogo1.png'
+            alt='Fotografia com um grupo de pessoas de nossa organização'
+          />
+          <C.Button text='Clique aqui e tenha um novo afilhado' />
+        </S.ToAdopteContainer>
+      </S.ThirdContainer>
 
       <h1>Home</h1>
     </>
--- src/pages/general/home/home.styles.ts
+++ src/pages/general/home/home.styles.ts
@@ -43,12 +43,82 @@ export const AboutUsContainer = styled.div`
   padding-top: 200px;
 `;
 
+export const AboutUsFirstContainer = styled.div`
+  text-align: center;
+  max-width: 700px;
+  padding: 20px;
+  margin: 0 auto;
+`;
+
+export const AboutUsSecondContainer = styled.div`
+  padding-top: 60px;
+  padding: 20px;
+  text-align: center;
+
+  @media ${deviceMinWidth.laptopS} {
+    margin: 0 auto;
+    display: grid;
+    text-align: unset;
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+    align-items: center;
+  }
+`;
+
+export const Col = styled.div``;
+
+export const ColText = styled.div`
+  padding-bottom: 40px;
+  @media ${deviceMinWidth.laptopS} {
+    padding-right: 20px;
+  }
+  @media ${deviceMinWidth.desktopS} {
+    margin-left: 340px;
+  }
+`;
+
+export const UsImg = styled.img`
+  width: 200px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  @media ${deviceMinWidth.laptopS} {
+    width: 400px;
+  }
+  @media ${deviceMinWidth.desktopS} {
+    width: 600px;
+  }
+`;
+
+export const ToAdopteContainer = styled.div`
+  text-align: center;
+  max-width: 700px;
+  padding: 20px;
+  padding-top: 80px;
+  margin: 0 auto;
+`;
+
 export const TitleAboutUsContainer = styled.div`
   padding-top: 200px;
 `;
 
+export const ToAdopteImg = styled.img`
+  width: 200px;
+
+  @media ${deviceMinWidth.laptopS} {
+    width: 400px;
+  }
+  @media ${deviceMinWidth.desktopS} {
+    width: 600px;
+  }
+`;
+
 export const SecondContainer = styled.div`
   background-color: var(--grayLight);
-  height: 100vh;
+  width: 100vw;
+`;
+
+export const ThirdContainer = styled.div`
+  background-color: var(--grayMedium);
   width: 100vw;
 `;
