50e03bea971a2f0330e6f4450a3a5fe6a8edcd13
bolodissenoura <suamaemedeu@outlook.com>
2022-08-07 23:52:24 -0300

    fix: parallax scroll on small devices

--- src/components/parallaxSection/parallaxSection.styles.ts
+++ src/components/parallaxSection/parallaxSection.styles.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
+import { deviceMinWidth } from '../../styles';
 
 export const Container = styled.div<{ image: string }>`
   min-height: 500px;
   width: 100%;
   background-image: url(${props => props.image});
   /* Create the parallax scrolling effect */
-  background-attachment: fixed;
+  @media ${deviceMinWidth.tabletL} {
+    background-attachment: fixed;
+  }
+
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
