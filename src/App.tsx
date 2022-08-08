1502c8852452b681e2058106fea8aa5d6b6d19f3
bolodissenoura <suamaemedeu@outlook.com>
2022-08-07 23:51:51 -0300

    feat: whatsapp button

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,13 @@ export function App() {
           <Route path='/' element={<Home />} />
         </Routes>
       </Router>
+      <a
+        href='https://api.whatsapp.com/send?phone=5516981278303'
+        className='float'
+        target='_blank'
+      >
+        <i className='fa fa-whatsapp my-float'></i>
+      </a>
       <GlobalStyles />
     </>
   );
